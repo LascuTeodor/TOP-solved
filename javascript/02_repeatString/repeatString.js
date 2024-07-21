fffec0ed201aeb7af58d839e1549fa3d85eaf04f
Lascu Teodor <lascuteodor15@gmail.com>
2024-07-21 15:46:44 +0100

    02_repeatString done

---
+++ javascript/02_repeatString/repeatString.js
@@ -0,0 +1,22 @@
+const repeatString = function(string, num) {
+
+    if(num < 0){
+        return "ERROR";
+    }
+
+    if(num === 0 ){
+        return "";
+    }
+
+    if(num === 1){
+        return string;
+    }
+    else{
+        return string + repeatString(string, num - 1)
+    }
+
+}
+
+
+// Do not edit below this line
+module.exports = repeatString;
