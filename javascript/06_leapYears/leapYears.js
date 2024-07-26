8477cb07b66cf95d719b09114a467fc3c9861619
Lascu Teodor <lascuteodor15@gmail.com>
2024-07-26 18:47:10 +0200

    06-leapYears done

---
+++ javascript/06_leapYears/leapYears.js
@@ -0,0 +1,9 @@
+const leapYears = function(yearToCheck) {
+    if(yearToCheck % 4 === 0 && (!(yearToCheck % 100 === 0) || yearToCheck % 400 === 0)){
+       return true
+    }
+    else return false
+};
+
+// Do not edit below this line
+module.exports = leapYears;
