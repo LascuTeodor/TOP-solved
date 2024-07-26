16a5a8d34bff0f137d12ee2beb4445ba678e2068
Lascu Teodor <lascuteodor15@gmail.com>
2024-07-26 18:12:52 +0200

    05-sumAll done

--- javascript/05_sumAll/sumAll.js
+++ javascript/05_sumAll/sumAll.js
@@ -1,17 +1,21 @@
 const sumAll = function(startNum,endNum) {
     let sum = 0;
-    if(startNum <= endNum){
-        for (let i = startNum; i <= endNum; i++) {
-            sum = sum + i;
-    }
-    }else{
-        for (let i = startNum; i >= endNum; i--) {
-            sum = sum + i;
+    if(Number.isInteger(startNum) && Number.isInteger(endNum) && startNum >= 0 && endNum >= 0){
+        if(startNum <= endNum){
+            for (let i = startNum; i <= endNum; i++) {
+                sum = sum + i;
         }
-    }
+        }else{
+            for (let i = startNum; i >= endNum; i--) {
+                sum = sum + i;
+            }
+        }
+        
+    }else return "ERROR"
     
     return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
