bf2c476a5664ce37fa1d3b35c28079f33cb1e8dc
Lascu Teodor <lascuteodor15@gmail.com>
2024-07-28 08:41:46 +0200

    09-palindromes 2 tests done, 6 to go

---
+++ javascript/09_palindromes/palindromes.js
@@ -0,0 +1,14 @@
+const palindromes = function (palindromeToCheck) {
+    let newStr = "";
+    let punctuationFilter = palindromeToCheck.replace(/[^\w]/g, '');
+    for (let i = punctuationFilter.length - 1; i >= 0 ; i--) {
+        newStr+=punctuationFilter[i];
+    }
+    if(punctuationFilter === newStr)
+        return true; 
+    else return false;
+   
+};
+
+// Do not edit below this line
+module.exports = palindromes;
